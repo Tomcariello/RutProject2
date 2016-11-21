26e8afa61b38a9c61e96c67fe3fb36f982b8ef78
Tomcariello <tomcariello@yahoo.com>
2016-11-21 15:27:06 -0500

    created usergoals association and some dummy data

--- controllers/bl_controller.js
+++ controllers/bl_controller.js
@@ -10,13 +10,38 @@ var sequelizeConnection = models.sequelize
 // We run this query so that we can drop our tables even though they have foreign keys
 sequelizeConnection.query('SET FOREIGN_KEY_CHECKS = 0')
 
-// make our tables
-// note: force:true drops the table if it already exists
-
+// make our tables; force:true drops the table if it already exists
 .then(function(){
   // return sequelizeConnection.sync({force:true})
 })
 
+
+//Create sequelize associations in the table
+
+//Assign user 1 goal 1
+models.Users.findOne({where: {id: 1} })
+  // with .then, we can work with this an instance and add a goal
+  .then(function(user){
+    return user.addGoals(1);
+  })
+
+models.Users.findOne({where: {id: 2} })
+  // with .then, we can work with this an instance and add a goal
+  .then(function(user){
+    return user.addGoals(2);
+  })
+
+models.Users.findOne({where: {id: 3} })
+  // with .then, we can work with this an instance and add a goal
+  .then(function(user){
+    return user.addGoals(4);
+  })
+
+models.Users.findOne({where: {id: 4} })
+  // with .then, we can work with this an instance and add a goal
+  .then(function(user){
+    return user.addGoals(3);
+  })
 
 
 //Establish page routing
