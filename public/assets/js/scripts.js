f4423bf58edfef36034250cf22988780c83169df
mnaumoski <naumoski.marija@gmail.com>
2016-11-23 20:23:35 -0500

    display as dashboard

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,6 +1,10 @@
 //Test connectivity per page
 console.log('hello world');
 
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
 // Features used on the browse page
 $(document).on('click','.card',function(){
   //kill card
