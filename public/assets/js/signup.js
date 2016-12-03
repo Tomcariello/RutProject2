2696a1ba0a3695ccc51bec6c3370acfcc0b61712
Tomcariello <tomcariello@yahoo.com>
2016-12-02 22:59:29 -0500

    fixed signup page personal form loading

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,15 +1,14 @@
-$(document).on('click', function(){
+$( document ).ready(function() {
 
-    $('#personal').click(function(){
-
+  $('#personal').click(function(){
     $('#signup_formB').fadeOut(700);
     $('#signup_formP').fadeIn(700);
+  });
 
-});
   $('#business').click(function(){
-
+    console.log('business was clicked');
     $('#signup_formP').fadeOut(700);
     $('#signup_formB').fadeIn(700);
-
   });
 });
+
